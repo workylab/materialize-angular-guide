c8007136fee5d6d8e05863fc80a3610e1180b6f2
Alejandro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-03-18 01:49:24 -0400

    Update project name to materialize angular guide.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/angular-generic-components-guide'),
+      dir: require('path').join(__dirname, '../coverage/materialize-angular-guide'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
