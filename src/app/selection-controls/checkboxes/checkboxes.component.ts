c14f9ffddd9026235d7aadeff29dec9a1cf760ee
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-07-13 13:14:21 -0400

    Removed selection controls.

--- src/app/selection-controls/checkboxes/checkboxes.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './checkboxes.component.html'
-})
-export class CheckboxesComponent {
-}
--- src/app/selection-controls/selection-controls.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './selection-controls.component.html'
-})
-export class SelectionControlsComponent {
-}
