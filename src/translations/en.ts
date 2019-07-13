a8398b4580465cca9e9997c869cb391f54767d67
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-07-13 13:12:55 -0400

    Added translation files.

---
+++ src/translations/en.ts
@@ -0,0 +1,31 @@
+import { Translations } from 'src/app/translation.model';
+
+export const translations: Translations = {
+  drawer: {
+    accordion: 'Accordion',
+    button: 'Button',
+    buttonToggle: 'Button Toggle',
+    calendar: 'Calendar',
+    checkbox: 'Checkbox',
+    checkboxList: 'Checkbox List',
+    collapsible: 'Collapsible',
+    datePicker: 'Date Picker',
+    drawer: 'Drawer',
+    dropdown: 'Dropdown',
+    icon: 'Icon',
+    input: 'Input',
+    label: 'Label',
+    list: 'List',
+    modal: 'Modal',
+    navbar: 'Navbar',
+    radio: 'Radio',
+    ripple: 'Ripple',
+    select: 'Select',
+    slider: 'Slider',
+    switch: 'Switch',
+    tab: 'Tab',
+    table: 'Table',
+    textarea: 'Text Area',
+    tooltip: 'Tooltip'
+  }
+};
