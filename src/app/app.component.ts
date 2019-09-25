bc1856f91a622eb52a3761c556d082ec9306226e
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-09-25 15:28:43 -0400

    Fixed redirection scroll.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private router: Router) {
+    this.router.events.subscribe(route => {
+      if (route instanceof NavigationEnd && !route.url.includes('#')) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 }
